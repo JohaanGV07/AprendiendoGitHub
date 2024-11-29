70f275d40a37bebc582d2737707cde94a2479c90
JohaanGV07 <johgamvid.alu.edu.gva.es>
2024-11-29 20:06:17 +0100

    Modificando ultimo print de despedir

--- Despedida.java
+++ Despedida.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Despedida {
   
-    public static void main(String[] args) {
-        
-        
+    public static void despedir(){
+        Scanner teclado= new Scanner(System.in);
+        System.out.println("Dime tu nombre");
+        String nombre=teclado.nextLine();
+       
+        System.out.println("Adios" + nombre + "Espero verte mas a menudo");
+        teclado.close();
     }
 }
--- Principal.java
+++ Principal.java
@@ -1,7 +1,7 @@
 public class Principal {
     public static void main(String[] args) {
         Saludo saludo= new Saludo();
-        Despedida despedida= new Despeedida();
+        Despedida despedida= new Despedida();
 
         saludo.saludar();
         despedida.despedir();
