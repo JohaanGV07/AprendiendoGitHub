db5d8f8ff5e01a1080f2593c54fe2e40bc104979
JohaanGV07 <johgamvid.alu.edu.gva.es>
2024-11-29 19:44:43 +0100

    Saludo.java y Despeida Agregado Principal.java

---
+++ Principal.java
@@ -0,0 +1,14 @@
+public class Principal {
+    public static void main(String[] args) {
+        Saludo saludo= new Saludo();
+        Despedida despedida= new Despeedida();
+
+        saludo.saludar();
+        despedida.despedir();
+
+
+
+
+
+    }
+}
